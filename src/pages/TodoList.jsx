a46bb5b13607c76c8c916d10cd014cada2f7e58f
Leovison <cleovison@gmail.com>
2024-01-13 16:48:11 +0800

    Edits

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -8,10 +8,7 @@ export default function TodoList({ list, update }) {
     update(tmp);
   };
 
-  
-
   function updates(e) {
- 
     if (e.target.innerText === "Edit") {
       e.target.innerText = "Save";
     } else {
@@ -23,20 +20,18 @@ export default function TodoList({ list, update }) {
       <div className={styles.title}>Task List</div>
       <ul className={styles.list}>
         {list.map((lis, index) => (
-          <>
-            <li key={lis?.id}>
-              <input
-                type="text"
-                value={lis?.text}
-                onChange={(e) => e.target.value}
-              />
+          <li key={lis?.id}>
+            <input
+              type="text"
+              value={lis?.text}
+              onChange={(e) => e.target.value}
+            />
 
-              <div className={styles.buttons}>
-                <button onClick={updates}>Edit</button>
-                <button onClick={() => removeElement(lis?.id)}>Delete</button>
-              </div>
-            </li>
-          </>
+            <div className={styles.buttons}>
+              <button onClick={updates}>Edit</button>
+              <button onClick={() => removeElement(lis?.id)}>Delete</button>
+            </div>
+          </li>
         ))}
       </ul>
     </>
